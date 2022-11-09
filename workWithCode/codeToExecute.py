737ab7765b0e17a816922cbad7556e97d4b04d0d
alexbailaEndava <117368299+alexbailaEndava@users.noreply.github.com>
2022-11-09 14:39:21 +0200

    Update codeToExecute.py

--- workWithCode/codeToExecute.py
+++ workWithCode/codeToExecute.py
@@ -1,17 +1,1 @@
 
-
-import sys
-
-if len(sys.argv) != 2:
-    print("Usage: python3 gen_add.py <filename>")
-    sys.exit(1)
-
-filename = sys.argv[1]
-
-with open(filename, "w") as f:
-    f.write("""
-def add(a, b):
-    return a + b
-
-print(add(2, 3))
-""")
